0bfbddcfdbc67a748d529f2b68dc575c132b3aa1
Chandan Reddy <chandan.reddy@betsol.com>
2022-05-12 17:25:21 +0530

    added DB URI

--- static/__init__.py
+++ static/__init__.py
@@ -16,7 +16,7 @@ cors = CORS()
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://YOUR_URI'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://adjjmagilpnpia:26ea2194332b7e6b40b1a76992597a86a0da485722931ff096421387bb0fb74c@ec2-35-168-194-15.compute-1.amazonaws.com:5432/d8isr97rh1h310'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     from static.todoApp.model.todo_list_model import Todo
     db.init_app(app)
